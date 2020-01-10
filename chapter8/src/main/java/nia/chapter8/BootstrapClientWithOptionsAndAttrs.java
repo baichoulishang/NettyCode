cb13798eae918ef4acc2f1092a909edda8e135b4
baichoulishang <baichoulishang@163.com>
2020-01-10 19:22:18 +0800

    添加了注释

--- chapter8/src/main/java/nia/chapter8/BootstrapClientWithOptionsAndAttrs.java
+++ chapter8/src/main/java/nia/chapter8/BootstrapClientWithOptionsAndAttrs.java
@@ -24,7 +24,7 @@ public class BootstrapClientWithOptionsAndAttrs {
      */
     public void bootstrap() {
         // 创建一个 AttributeKey 以标识该属性
-        final AttributeKey<Integer> id = AttributeKey.newInstance("ID");
+        final AttributeKey<Integer> key = AttributeKey.newInstance("ID");
         // 创建一个 Bootstrap 类的实例以创建客户端 Channel 并连接它们
         Bootstrap bootstrap = new Bootstrap();
         // 设置 EventLoopGroup，其提供了用以处理 Channel 事件的 EventLoop
@@ -35,17 +35,14 @@ public class BootstrapClientWithOptionsAndAttrs {
                         // 设置用以处理 Channel 的 I/O 以及数据的 ChannelInboundHandler
                         new SimpleChannelInboundHandler<ByteBuf>() {
                             @Override
-                            public void channelRegistered(ChannelHandlerContext ctx)
-                                    throws Exception {
+                            public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
                                 // 使用 AttributeKey 检索属性以及它的值
-                                Integer idValue = ctx.channel().attr(id).get();
+                                Integer idValue = ctx.channel().attr(key).get();
                                 // do something with the idValue
                             }
 
                             @Override
-                            protected void channelRead0(
-                                    ChannelHandlerContext channelHandlerContext,
-                                    ByteBuf byteBuf) throws Exception {
+                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
                                 System.out.println("Received data");
                             }
                         }
@@ -53,11 +50,10 @@ public class BootstrapClientWithOptionsAndAttrs {
         // 设置 ChannelOption，其将在 connect()或者bind()方法被调用时被设置到已经创建的 Channel 上
         bootstrap.option(ChannelOption.SO_KEEPALIVE, true)
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
-        // 存储该 id 属性
-        bootstrap.attr(id, 123456);
+        // 存储该 key 属性
+        bootstrap.attr(key, 123456);
         // 使用配置好的 Bootstrap 实例连接到远程主机
-        ChannelFuture future = bootstrap.connect(
-                new InetSocketAddress("www.manning.com", 80));
+        ChannelFuture future = bootstrap.connect(new InetSocketAddress("www.manning.com", 80));
         future.syncUninterruptibly();
     }
 }
--- chapter8/src/main/java/nia/chapter8/BootstrapDatagramChannel.java
+++ chapter8/src/main/java/nia/chapter8/BootstrapDatagramChannel.java
@@ -1,10 +1,7 @@
 package nia.chapter8;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.oio.OioDatagramChannel;
@@ -23,27 +20,25 @@ public class BootstrapDatagramChannel {
      * 代码清单 8-8 使用 Bootstrap 和 DatagramChannel
      */
     public void bootstrap() {
+        EventLoopGroup group = new OioEventLoopGroup();
         // 创建一个 Bootstrap 的实例以创建和绑定新的数据报 Channel
         Bootstrap bootstrap = new Bootstrap();
         // 设置 EventLoopGroup，其提供了用以处理 Channel 事件的 EventLoop
-        bootstrap.group(new OioEventLoopGroup()).channel(
+        bootstrap.group(group)
                 // 指定 Channel 的实现
-                OioDatagramChannel.class).handler(
+                .channel(OioDatagramChannel.class)
                 // 设置用以处理 Channel 的 I/O 以及数据的 ChannelInboundHandler
-                new SimpleChannelInboundHandler<DatagramPacket>() {
+                .handler(new SimpleChannelInboundHandler<DatagramPacket>() {
                     @Override
-                    public void channelRead0(ChannelHandlerContext ctx,
-                                             DatagramPacket msg) throws Exception {
+                    public void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
                         // Do something with the packet
                     }
-                }
-        );
+                });
         // 调用 bind() 方法，因为该协议是无连接的
         ChannelFuture future = bootstrap.bind(new InetSocketAddress(0));
         future.addListener(new ChannelFutureListener() {
             @Override
-            public void operationComplete(ChannelFuture channelFuture)
-                    throws Exception {
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
                 if (channelFuture.isSuccess()) {
                     System.out.println("Channel bound");
                 } else {
